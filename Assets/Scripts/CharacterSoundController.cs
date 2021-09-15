5444e1267bb554d0c66a17cd7e3b08ce51e14a89
jihanaferiansyah <jihanaferian@gmail.com>
2021-09-15 16:55:03 +0700

    Third Unity

---
+++ Assets/Scripts/CharacterSoundController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSoundController : MonoBehaviour
+{
+    public AudioClip jump;
+
+    private AudioSource audioPlayer;
+
+    private void Start()
+    {
+        audioPlayer = GetComponent<AudioSource>();
+    }
+
+
+    // Update is called once per frame
+    public void PlayJump()
+    {
+        audioPlayer.PlayOneShot(jump);
+    }
+}
